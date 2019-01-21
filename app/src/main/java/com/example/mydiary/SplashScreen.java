edfa5de5fff2da5d77a56532b475cb0615d8e21d
nafsan <iamnafsan@gmail.com>
2019-01-21 22:43:43 +0600

    Android Studio Project

---
+++ app/src/main/java/com/example/mydiary/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.example.mydiary;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private int progress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        progressBar = findViewById(R.id.progressBarId);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doWork();
+                startActivity(new Intent(SplashScreen.this,LoginActivity.class));
+                finish();
+            }
+        });
+        thread.start();
+    }
+    public void doWork()
+    {
+        for(progress = 20;progress<100;progress+=20)
+        {
+
+            try {
+                Thread.sleep(1000);
+                progressBar.setProgress(progress);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
